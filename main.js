1ee338ad43949f39f4afc08f9fa24e937cba0f44
Guido Giglioli <guido.tobugroup@gmail.com>
2026-05-17 18:37:35 +0200

    iniziato pseudocodice per funzione pariODispari()

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ Creare una funzione per capire se la parola inserita è palindroma */
 
 /*
 PALINDROMA
-Un parametro che é la parola inserita in input
-casto a stringa perché sono un cane
-creo un'array che é input rovesciato
-creo una stringa che é input rovesciato
-poi faccio un controllo tra stringa rovesciata e input
+1) Un parametro che é la parola inserita in input
+2) casto a stringa perché sono un cane
+3) creo un'array che é input rovesciato
+4) creo una stringa che é input rovesciato
+5) poi faccio un controllo tra stringa rovesciata e input
 */
 
 
-console.log(isPalindrome(prompt("Inserisci la stringa da controllare.")));
+//console.log(isPalindrome(prompt("Inserisci la stringa da controllare.")));
 
 function isPalindrome(input) {
     input = String(input);
@@ -22,3 +22,13 @@ function isPalindrome(input) {
     let reversedStr = reversedArr.join("");
     return reversedStr === input;
 }
+
+
+
+/* 
+PARI E DISPARI
+1) due parametri: 
+- parDis castato a stringa tolowercase() e validato con typeOf(parDis)
+- numero da 1 a 5, validato con Number.isInteger(numero)
+2) 
+*/

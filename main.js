55b09cdcee4b85de8863503f7b442a51092e4da7
Guido Giglioli <guido.tobugroup@gmail.com>
2026-05-17 18:10:04 +0200

    blueprint prima funzione

---
+++ main.js
@@ -0,0 +1,20 @@
+/*Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma */
+
+/*Un parametro che é la parola inserita in input
+casto a stringa perché sono un cane
+creo un'array di appoggio 
+creo una stringa di appoggio che é input rovesciato
+poi faccio un controllo tra stringa rovesciata e input
+
+*/
+const input = 'ciccio';
+
+function isPalindrome (input) {
+    input = String(input);
+    let reversedArr = [];
+    let reversedStr = '';
+    for (let i = 0; i < input.lenght; i++) {
+        
+    }
+}
